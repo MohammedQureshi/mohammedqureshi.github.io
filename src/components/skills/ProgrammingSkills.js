72b9362c25ba4ee404eede3f87beb66141e78549
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-09 21:07:01 +0100

    Working site

---
+++ src/components/skills/ProgrammingSkills.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './ProgrammingSkills.scss'
+
+function ProgrammingSkills(){
+    return(
+        <div className="programmingskills">
+            <div className="programmingskills-header">
+                <div className="circle" />
+                <div className="circle" />
+                <div className="circle" />
+            </div>
+            <div className="programmingskills-container">
+                <h1>Skills</h1>
+                <div className="programmingskills-skils">
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProgrammingSkills
--- src/pages/home_page/HomePage.js
+++ src/pages/home_page/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ProgrammingSkills from '../../components/skills/ProgrammingSkills';
 import './HomePage.scss'
 
 function HomePage() {
@@ -6,7 +7,7 @@ function HomePage() {
         let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) *pageNumber;
         window.scrollTo({
             top: height,
-            behavior: 'smooth'     
+            behavior: 'smooth'    
         })
     }
     return (
@@ -22,14 +23,19 @@ function HomePage() {
                 </div>
             </div>
             <div className="AboutMeSection">
-                <div className="MeStandingTwo" />
+                <div className="AboutMeText">
+                    <h1>About Me</h1>
+                    <p>Full-Stack Software Engineer with Bachelor's in Computer Science with a passion for technology and exploring current and upcoming technology. Interested in software analysis, development and software engineering. I also have an interest in other IT sectors such as cybersecurity and data development.</p>
+                </div>
+                <div className="MeOnLaptop" />
                 <div className="scroll-about-me" onClick={() => scrollPage(2)}>
                         <p> Check out my skills </p>
                         <i className="fas fa-chevron-down"></i>
                 </div>
             </div>
             <div className="Skills">
-                
+            <div className="MeSitting" />
+                <div className="SkillSet" ><ProgrammingSkills/></div>
                 <div className="scroll-about-me" onClick={() => scrollPage(0)}>
                         <p> Back to the top </p>
                         <i className="fas fa-chevron-up"></i>
