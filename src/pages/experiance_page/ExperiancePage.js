b45e4ded723ef0bf842cfd3f76a63c3cc2d7dfef
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-17 16:22:11 +0100

    Add education page and new font and setup mobile view

--- src/pages/experiance_page/ExperiancePage.js
+++ src/pages/experiance_page/ExperiancePage.js
@@ -1,6 +1,7 @@
 import React,{useRef} from 'react'
 import './Experiance.scss'
 import WorkExperienceList from '../../components/work-experience/WorkExperience';
+import edgehilluniversity from '../../images/company_logos/edgehilluniversity.png'
 
 function ExperiancePage() {
     const ref = useRef(null);
@@ -30,7 +31,24 @@ function ExperiancePage() {
                 </div>
             </div>
             
-            <div className="education" ref={education} ></div>
+            <div className="education" ref={education} >
+                <div className="university">
+                    <img src={edgehilluniversity} alt="EdgeHillLogo" />
+                    <h1 className="university-title"> Edge Hill University </h1>
+                </div>
+                <div className="award">
+                    <h1>BACHELOR OF SCIENCE WITH HONOURS IN COMPUTER SCIENCE</h1>
+                    <h2>Second Class Honours (Division One)</h2>
+                    <p className="bold">Modules</p>
+                    <p>Foundations of Computer Science, Digital World, Programming, Web Design and Development, Databases, Employability, Data Analytics, Statistical Inference, Introduction to Security, Software Engineering, Research and Development Project, Complex System, Research and Development Methods, Mathematical Models, Internet Security.</p>
+                    <p className="bold">Research and Development Project</p>
+                    <p>I conducted research on product reviews for a set company and set up a program where I will be able to use AI to analyse the review and detect the emotions that It gives off to help support business in improving their product. Features will be added to automatically analyse certain features within the product itself.</p>
+                </div>
+                <div className="scroll-about-me" onClick={goToEducation}>
+                        <p> Go To Licenses And Certifications </p>
+                        <i className="fas fa-chevron-down"></i>
+                </div>
+            </div>
         </div>
     )
 }
