c8749b144e19789cba04c5dc284f0e269535cfb9
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-17 14:23:55 +0100

    Fix positioning of man to div

--- src/pages/experiance_page/ExperiancePage.js
+++ src/pages/experiance_page/ExperiancePage.js
@@ -17,12 +17,12 @@ function ExperiancePage() {
                 <div className="work-experience">
                     <button onClick={() => scroll(-420)} className="left-slider"><i className="fas fa-chevron-left" /></button>
                     <div className="experience-cards">
+                        <div className="MeLeaning" />
                         <div className="experience-container" ref={ref} >
                             <div className="work"><WorkExperienceList /></div>
                         </div>
                     </div>
                     <button onClick={() => scroll(420)} className="right-slider"><i className="fas fa-chevron-right" /></button>
-                    <div className="MeLeaning" />
                 </div>
                 <div className="scroll-about-me" onClick={goToEducation}>
                         <p> Go to Education </p>
